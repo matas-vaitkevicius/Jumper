ecdffe2e0ab6fcfdfce86881b42390c98e6f51c6
Matas Vaitkevicius <mataswork@gmail.com>
2020-03-13 18:35:28 +0200

    Secondary commit, added empty projects, mechanism and tests to traverse array

---
+++ Jumper.Core.Tests/CalculatorTest.cs
@@ -0,0 +1,97 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Jumper.Core.Tests
+{
+    public class Tests
+    {
+        private Calculator Calculator { get; set; }
+        [SetUp]
+        public void Setup()
+        {
+            this.Calculator = new Calculator();
+        }
+
+        [Test]
+        [TestCaseSource("CanBeCompletedTestData")]
+        public void CanBeCompteted(List<int> data, bool expected)
+        {
+            var actual = this.Calculator.HasPathToFirstElement(data);
+            Assert.AreEqual(actual, expected);
+        }
+
+        public static IEnumerable<TestCaseData> CanBeCompletedTestData()
+        {
+            yield return new TestCaseData(new List<int> { 0 }, true);
+            yield return new TestCaseData(new List<int> { 1, 0 }, true);
+            yield return new TestCaseData(new List<int> { 2, 0 }, true);
+            yield return new TestCaseData(new List<int> { 1, 1, 0 }, true);
+            yield return new TestCaseData(new List<int> { 2, 0, 0 }, true);
+            yield return new TestCaseData(new List<int> { 2, 1, 0 }, true);
+            yield return new TestCaseData(new List<int> { 3, 0, 0 }, true);
+            yield return new TestCaseData(new List<int> { 1, -1, 0 }, false);
+            yield return new TestCaseData(new List<int> { 2, -1, 0 }, true);
+            yield return new TestCaseData(new List<int> { 2, 0, 0, 0 }, false);
+            yield return new TestCaseData(new List<int> { 2, 1, 0, 0 }, false);
+            yield return new TestCaseData(new List<int> { 1, 2, 0, 0 }, true);
+            yield return new TestCaseData(new List<int> { 1, 2, 0, 3, 0, 2, 0 }, true);
+            yield return new TestCaseData(new List<int> { 1, 2, 0, 1, 0, 2, 0 }, false);
+            yield return new TestCaseData(new List<int> { 1, 2, 0, -1, 0, 2, 0 }, false);
+            yield return new TestCaseData(new List<int> { 1, 2, 1, -1, 0, 2, 0 }, false);
+
+        }
+
+        [Test]
+        [TestCaseSource("GetLongestPathTestData")]
+        public void GetLongestPath(List<int> data, Dictionary<int, int> expected)
+        {
+            var actual = this.Calculator.GetLongestPath(data);
+            Assert.AreEqual(actual, expected);
+        }
+
+        public static IEnumerable<TestCaseData> GetLongestPathTestData()
+        {
+            yield return new TestCaseData(new List<int> { 0 }, new Dictionary<int, int> { { 0, 0 } });
+            yield return new TestCaseData(new List<int> { 1, 0 }, new Dictionary<int, int> { { 1, 0 }, { 0, 1 } });
+            yield return new TestCaseData(new List<int> { 2, 0 }, new Dictionary<int, int> { { 1, 0 }, { 0, 1 } });
+            yield return new TestCaseData(new List<int> { 2, 1, 0 }, new Dictionary<int, int> { { 2, 0 }, { 1, 1 }, { 0, 1 } });
+            yield return new TestCaseData(new List<int> { 2, 0, 0 }, new Dictionary<int, int> { { 2, 0 }, { 0, 2 } });
+            yield return new TestCaseData(new List<int> { 3, 0, 0 }, new Dictionary<int, int> { { 2, 0 }, { 0, 2 } });
+            yield return new TestCaseData(new List<int> { 1, -1, 0 }, null);
+            yield return new TestCaseData(new List<int> { 2, -1, 0 }, new Dictionary<int, int> { { 2, 0 }, { 0, 2 } });
+            yield return new TestCaseData(new List<int> { 2, 0, 0, 0 }, null);
+            yield return new TestCaseData(new List<int> { 2, 1, 0, 0 }, null);
+            yield return new TestCaseData(new List<int> { 1, 2, 0, 0 }, new Dictionary<int, int> { { 3, 0 }, { 1, 2 }, { 0, 1 } });
+            yield return new TestCaseData(new List<int> { 1, 2, 0, 3, 0, 2, 0 }, new Dictionary<int, int> { { 6, 0 }, { 5, 1 }, { 3, 2 }, { 1, 2 }, { 0, 1 } });
+            yield return new TestCaseData(new List<int> { 1, 2, 0, 1, 0, 2, 0 }, null);
+            yield return new TestCaseData(new List<int> { 1, 2, 0, -1, 0, 2, 0 }, null);
+            yield return new TestCaseData(new List<int> { 1, 2, 1, -1, 0, 2, 0 }, null);
+            yield return new TestCaseData(new List<int> { 6, 1, 1, 1, 1, 1, 6 }, new Dictionary<int, int> { { 6, 6 }, { 5, 1 }, { 4, 1 }, { 3, 1 }, { 2, 1 }, { 1, 1 }, { 0, 1 } });
+
+        }
+
+        [Test]
+        [TestCaseSource("GetShortestPathTestData")]
+        public void GetShortestPath(List<int> data, Dictionary<int, int> longestPath, List<int> expected)
+        {
+            var actual = this.Calculator.GetShortestPath(data, longestPath);
+            Assert.AreEqual(actual, expected);
+        }
+
+        public static IEnumerable<TestCaseData> GetShortestPathTestData()
+        {
+            yield return new TestCaseData(new List<int> { 0 }, new Dictionary<int, int> { { 0, 0 } }, new List<int> { 0 });
+            yield return new TestCaseData(new List<int> { 1, 0 }, new Dictionary<int, int> { { 1, 0 }, { 0, 1 } }, new List<int> { 0, 1 });
+            yield return new TestCaseData(new List<int> { 2, 0 }, new Dictionary<int, int> { { 1, 0 }, { 0, 1 } }, new List<int> { 0, 1 });
+            yield return new TestCaseData(new List<int> { 2, 1, 0 }, new Dictionary<int, int> { { 2, 0 }, { 1, 1 }, { 0, 1 } }, new List<int> { 0, 2 });
+            yield return new TestCaseData(new List<int> { 2, 0, 0 }, new Dictionary<int, int> { { 2, 0 }, { 0, 2 } }, new List<int> { 0, 2 });
+            yield return new TestCaseData(new List<int> { 3, 0, 0 }, new Dictionary<int, int> { { 2, 0 }, { 0, 2 } }, new List<int> { 0, 2 });
+            yield return new TestCaseData(new List<int> { 2, -1, 0 }, new Dictionary<int, int> { { 2, 0 }, { 0, 2 } }, new List<int> { 0, 2 });
+            yield return new TestCaseData(new List<int> { 1, 2, 0, 0 }, new Dictionary<int, int> { { 3, 0 }, { 1, 2 }, { 0, 1 } }, new List<int> { 0, 1, 3 });
+            yield return new TestCaseData(new List<int> { 1, 2, 0, 3, 0, 2, 0 }, new Dictionary<int, int> { { 6, 0 }, { 5, 1 }, { 3, 2 }, { 1, 2 }, { 0, 1 } }, new List<int> { 0, 1, 3, 6 });
+            yield return new TestCaseData(new List<int> { 6, 1, 1, 1, 1, 1, 6 }, new Dictionary<int, int> { { 6, 6 }, { 5, 1 }, { 4, 1 }, { 3, 1 }, { 2, 1 }, { 1, 1 }, { 0, 1 } }, new List<int> { 0, 6 });
+            yield return new TestCaseData(new List<int> { 3, 1, 1, 2, 1, 1, 6 }, new Dictionary<int, int> { { 6, 6 }, { 5, 1 }, { 4, 1 }, { 3, 1 }, { 2, 1 }, { 1, 1 }, { 0, 1 } }, new List<int> { 0, 3, 5, 6 });
+
+        }
+    }
+}
---
+++ Jumper.Core/Calculator.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Jumper.Core
+{
+    public class Calculator
+    {
+        public bool HasPathToFirstElement(List<int> input)
+        {
+            if (input.Count == 1) return true;
+
+            var jumpToTopPathHavingElementDistance = 1;
+            for (int i = input.Count - 2; i >= 0; i--)
+            {
+                if (input[i] >= jumpToTopPathHavingElementDistance)
+                {
+                    jumpToTopPathHavingElementDistance = 1;
+                }
+                else
+                {
+                    jumpToTopPathHavingElementDistance++;
+                }
+
+                if (i == 0 && input[i] >= jumpToTopPathHavingElementDistance) 
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public Dictionary<int,int> GetLongestPath(List<int> input) 
+        {
+            var path =  new Dictionary<int, int> { { input.Count-1, input[input.Count - 1] } } ;
+            if (input.Count == 1) return path;
+
+            var jumpToTopPathHavingElementDistance = 1;
+            for (int i = input.Count - 2; i >= 0; i--)
+            {
+                if (input[i] >= jumpToTopPathHavingElementDistance)
+                {
+                    path.Add(i, jumpToTopPathHavingElementDistance);
+                    jumpToTopPathHavingElementDistance = 1;
+                }
+                else
+                {
+                    jumpToTopPathHavingElementDistance++;
+                }
+
+                if (i == 0 && input[i] >= jumpToTopPathHavingElementDistance)
+                {
+                    return path;
+                }
+            }
+
+            return null;
+        }
+
+        public object GetShortestPath(List<int> data, Dictionary<int, int> longestPath)
+        {
+            var currentElement = 0;
+            var shortestPath = new List<int> { currentElement };
+            while (currentElement != data.Count - 1) 
+            {
+                var longestPotentialJump = data[currentElement];
+                while (!longestPath.ContainsKey(currentElement + longestPotentialJump))
+                {
+                    longestPotentialJump--;
+                }
+                shortestPath.Add(currentElement + longestPotentialJump);
+                currentElement += longestPotentialJump;
+            }
+
+            return shortestPath;
+        }
+    }
+}
